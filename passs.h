bc1a7803cf0fe7815cdb3745f6ba6ebbc8da48cd
Kritigh62-bit <Kritigh62@gmail.com>
2026-04-29 22:29:19 +0545

    Delete passs.h

--- passs.h
+++
@@ -1,8 +0,0 @@
-#ifndef PASS_H 
-#define PASSS_H
-
-int buildCharacterPool(char all[], int useUpper, int useLower, int useNumber, int useSymbols);
-void generatePassword(char all[], int index, int length, char password[]);
-void saveToFile(char password[]);
-
-#endif 
